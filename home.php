d216cf65bb78830027850a92e5b00b0f19982737
lucasvleite <lucasvleite@outlook.com>
2018-09-24 18:46:42 -0300

    ajeitando banco de dados

--- home.php
+++ home.php
@@ -1,15 +1,15 @@
 <?php
 
-// A sessão precisa ser iniciada em cada página diferente
-if (!isset($_SESSION)) session_start();
+// // A sessão precisa ser iniciada em cada página diferente
+// if (!isset($_SESSION)) session_start();
     
-// Verifica se não há a variável da sessão que identifica o usuário
-if (!isset($_SESSION['user'])) {
-    // Destrói a sessão por segurança
-    session_destroy();
-    // Redireciona o visitante de volta pro login
-    header("Location: login.php"); exit;
-}
+// // Verifica se não há a variável da sessão que identifica o usuário
+// if (!isset($_SESSION['user'])) {
+//     // Destrói a sessão por segurança
+//     session_destroy();
+//     // Redireciona o visitante de volta pro login
+//     header("Location: login.php"); exit;
+// }
 
 include("include/top.php");
 
